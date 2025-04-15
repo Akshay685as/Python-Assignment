7c6f02e55fb60adad8e4c022b64a1b810cbfcd8b
Akshay685as <akshay.mba09@iimamritsar.ac.in>
2025-04-15 12:55:00 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,11 @@
+# task2_greeting.py
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate full name
+full_name = first_name + " " + last_name
+
+# Display greeting
+print(f"Hello, {full_name}! Welcome to Python programming ")
